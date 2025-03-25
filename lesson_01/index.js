323154bdad6b1ac8b40ad113efcc31117b0a89e8
JuliaAme <kljukv@gmail.com>
2025-03-25 19:58:06 +0300

    types

---
+++ lesson_01/index.js
@@ -0,0 +1,13 @@
+function summ (a, b){
+ return a + b
+}
+// console.log(summ(2, 3))
+
+const summ2 = (a, b) => a + b
+// console.log(summ2(5, 4));
+const summ3 = (a, b) => {
+    return a + b
+}
+// console.log(summ3(1, 2));
+
+
---
+++ lesson_01/types.js
@@ -0,0 +1,7 @@
+// String, number, boolean, null, undefined, BigInt, object, symbol
+const str = 'julia';
+console.log(str.split(''))
+console.log(str.length);
+console.log(str.includes('j', 0));
+console.log(str.indexOf('a', 1));
+console.log(str.toUpperCase());
